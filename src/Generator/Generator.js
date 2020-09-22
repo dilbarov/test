1b8aa50afce1e39d83df8154e23ffb8971a942a3
Yakovenko-Sergey3000 <sergeyyakovenko15@gmail.com>
2020-09-22 23:39:43 +0500

    add genarate functions

--- src/Generator/Generator.js
+++ src/Generator/Generator.js
@@ -1,8 +1,35 @@
 import React from "react";
-import { Button } from "@skbkontur/react-ui";
+import { Button  } from "@skbkontur/react-ui";
 import "./Generator.css";
 
 export const Generator = () => {
+
+    function generateInn(type){
+
+        function randomNumber(size) {
+          let i = Math.random().toString().slice(2, size);
+          return +i;
+        } 
+
+      if (type === 'ul') {
+         const ul =  randomNumber(12)
+         console.log(ul);
+      } else {
+        const fl =  randomNumber(14)
+         console.log(fl);
+
+      }
+    }
+
+    function generateKpp() {
+        const i = Math.random().toString().slice(2, 11);
+       
+    }
+
+    
+
+    
+  
   return (
     <div className={"test"}>
       <Button use={"primary"}>Test</Button>
