0e3c10e2e58e4e50c1449ef9380fd156945b63b4
Yakovenko-Sergey3000 <sergeyyakovenko15@gmail.com>
2020-09-23 14:32:49 +0500

    tranfer functions on generator.utils.js

--- src/Generator/Generator.js
+++ src/Generator/Generator.js
@@ -4,27 +4,7 @@ import "./Generator.css";
 
 export const Generator = () => {
 
-    function generateInn(type){
-
-        function randomNumber(size) {
-          let i = Math.random().toString().slice(2, size);
-          return +i;
-        } 
-
-      if (type === 'ul') {
-         const ul =  randomNumber(12)
-         console.log(ul);
-      } else {
-        const fl =  randomNumber(14)
-         console.log(fl);
-
-      }
-    }
-
-    function generateKpp() {
-        const i = Math.random().toString().slice(2, 11);
-       
-    }
+    
 
     
 
--- utils/generator.utils.js
+++ utils/generator.utils.js
@@ -2,10 +2,26 @@
  * Принимает type который может быть ul или fl
  * @param type
  */
+
+function randomNumber(size) {
+    let i = Math.random().toString().slice(2, size);
+    return i;
+  } 
+
 export const generateInn = (type) => {
-    /* здесь должен быть твой код*/
+
+    if (type === 'ul') {
+        const ul =  randomNumber(12)
+        console.log(ul);
+     } else {
+       const fl =  randomNumber(14)
+        console.log(fl);
+   
+     }
 };
 
 export const generateKpp = ()=> {
-    /* здесь должен быть твой код*/
+    const i = Math.random().toString().slice(2, 11);
 };
+
+
