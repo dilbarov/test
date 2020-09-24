3867394680951f916a8f3900d76b4a409930bf7f
Yakovenko-Sergey3000 <sergeyyakovenko15@gmail.com>
2020-09-24 14:50:31 +0500

    add vallid INN on zero

--- src/Generator/Generator.js
+++ src/Generator/Generator.js
@@ -1,17 +1,22 @@
 import React from "react";
-import { Button  } from "@skbkontur/react-ui";
+import { Autocomplete, Button, Select  } from "@skbkontur/react-ui";
 import "./Generator.css";
 
 export const Generator = () => {
 
     
-
+const state = () => {
+  return ['Физическое лицо', 'Юредическое лицо']
+}
     
 
     
   
   return (
     <div className={"test"}>
+     <Select items={state()}/>
+      <div>ИНН<Autocomplete className='input' readOnly/></div>
+      <div>КПП<Autocomplete className='input' readOnly/></div>
       <Button use={"primary"}>Test</Button>
     </div>
   );
--- utils/generator.utils.js
+++ utils/generator.utils.js
@@ -1,27 +1,46 @@
 /**
  * Принимает type который может быть ul или fl
- * @param type
+ * @param type 
  */
 
-function randomNumber(size) {
+const randomNumber = (size) => {
     let i = Math.random().toString().slice(2, size);
     return i;
   } 
+
+  const validInn = (inn) => {
+    if (inn[0] === 0 && inn[1] === 0) {
+      return false;
+    } else {
+      return true
+    }
+    
+  }
 
 export const generateInn = (type) => {
 
     if (type === 'ul') {
         const ul =  randomNumber(12)
-        console.log(ul);
-     } else {
+        const innValid = validInn(ul);
+        
+        
+        
+
+        return ul;
+        
+     } else  if (type === 'fl' ){
        const fl =  randomNumber(14)
-        console.log(fl);
+       const innValid = validInn(fl);
+       
+       
+        return fl;
    
      }
 };
 
 export const generateKpp = ()=> {
     const i = Math.random().toString().slice(2, 11);
+    return i;
 };
 
 
